b4dc9fea7890779de9ea2e223759c4f712a5a90d
leolassence <leolassence@gmail.com>
2016-02-21 16:46:21 +0000

    finished chapter 6

--- rails_app/test/models/user_test.rb
+++ rails_app/test/models/user_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @user = Users.new(name: "Exemple User", email: "user@exemple.com",
+                     password: "foobar", password_confirmation: "foobar")
+  end
+  
+  test "should be valid" do
+    assert @user.valid?
+  end
+  
+  test "name should be to long" do
+    @user.name = "a" * 50
+    assert_not @user.valid?
+  end
+  
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+  
 end
